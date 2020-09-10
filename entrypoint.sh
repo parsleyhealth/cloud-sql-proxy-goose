1721635c67f62d3d3ac5d068f03d0c5a92ffe677
Corbin Phelps <corbin@parsleyhealth.com>
2020-09-10 13:09:33 -0400

    Reworked entrypoint.sh and run-with-retry.sh to return correct error codes
    Signed-off-by: Corbin Phelps <corbin@parsleyhealth.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,15 @@
 # Save credentials to disk
 echo $GCP_CREDENTIAL_JSON > /tmp/credentials.json
 
-# Start cloudsql proxy and run goose retry script
-/cloud_sql_proxy -credential_file /tmp/credentials.json -instances=$CLOUDSQL_INSTANCE & /run-with-retry.sh
+# Start a headless cloudsql proxy and capture pid
+/cloud_sql_proxy -credential_file /tmp/credentials.json -instances=$CLOUDSQL_INSTANCE & echo $! > /tmp/cloud_sql_proxy.pid
+
+# Run goose command with retry and capture result
+/run-with-retry.sh
+result=$?
+
+# shut down cloudsql pid
+kill -s TERM $(cat /tmp/cloud_sql_proxy.pid)
+
+# Exit with goose retry result
+exit $result
